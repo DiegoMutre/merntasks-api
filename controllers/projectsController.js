29895b9f228e85d7e79f5c5291814288b9209e8b
DiegoMutre <tysonmutre10@gmail.com>
2021-02-16 09:01:39 -0500

    feat: Save projects with creator id in projectsController.

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -3,6 +3,8 @@ const Project = require("../models/Project");
 exports.createProject = async (req, res) => {
     try {
         const project = new Project(req.body);
+        // Save the creator
+        project.creator = req.user.id;
         await project.save();
         res.json(project);
     } catch (error) {
