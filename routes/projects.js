2fc53c26e05f0550ff203d6b648dfe71e24e6a3c
DiegoMutre <tysonmutre10@gmail.com>
2021-02-16 09:00:54 -0500

    feat: Use authentication middleware in projects routes.

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { createProject } = require("../controllers/projectsController");
+const authentication = require("../middlewares/authentication");
 const router = express.Router();
 
 // api/projects
-router.post("/", createProject);
-
+router.post("/", authentication, createProject);
 module.exports = router;
