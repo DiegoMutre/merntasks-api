f077a7b7751f50526b28128d7dde4997d7500ad0
DiegoMutre <tysonmutre10@gmail.com>
2021-02-16 07:03:33 -0500

    feat: Validate unique user.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,25 @@
 const User = require("../models/User");
 
 exports.createUser = async (req, res) => {
+    const { email, password } = req.body;
+
     try {
+        // Validate unique user
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(404).json({ msg: "User already exists" });
+        }
+
         // Create new user
-        let user = new User(req.body);
+        user = new User(req.body);
 
         // Save new user
         await user.save();
 
-        res.json("User created successfully");
+        res.json({ msg: "User created successfully" });
     } catch (error) {
         console.error(error);
-        res.status(404).json("There was a mistake");
+        res.status(404).json({ msg: "There was a mistake" });
     }
 };
