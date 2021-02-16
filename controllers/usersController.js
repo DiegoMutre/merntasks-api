4c9f41009670bbc0dd5af2d2d0357b4171330074
DiegoMutre <tysonmutre10@gmail.com>
2021-02-16 06:55:18 -0500

    feat: Insert user in the DB.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,16 @@
-exports.createUser = (req, res) => {
-    console.log(req.body);
+const User = require("../models/User");
+
+exports.createUser = async (req, res) => {
+    try {
+        // Create new user
+        let user = new User(req.body);
+
+        // Save new user
+        await user.save();
+
+        res.json("User created successfully");
+    } catch (error) {
+        console.error(error);
+        res.status(404).json("There was a mistake");
+    }
 };
