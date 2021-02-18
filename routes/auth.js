9ff7351017ed620bfb9d04dc1c02cc7e22a05ba7
DiegoMutre <tysonmutre10@gmail.com>
2021-02-18 05:49:15 -0500

    feat: Remove validation in auth routing.

--- routes/auth.js
+++ routes/auth.js
@@ -6,13 +6,7 @@ const authentication = require("../middlewares/authentication");
 const router = express.Router();
 
 // /api/auth
-router.post("/", [
-    check("email", "Add a valid email").isEmail(),
-    check("password", "Password must be at least 6 characters long").isLength({
-        min: 6,
-    }),
-    authUser,
-]);
+router.post("/", [authUser]);
 
 router.get("/", authentication, getUser);
 
